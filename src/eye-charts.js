d4b9742836e9da7980f7279e8137dfbae4a1e0f0
Ian Nastajus <ian.nastajus@gmail.com>
2020-01-31 23:05:44 -0500

    cleanup

--- src/eye-charts.js
+++ src/eye-charts.js
@@ -68,29 +68,10 @@ function uniqueInOrder(x) {
 /**
  * courtesy: https://stackoverflow.com/questions/1337419/how-do-you-convert-numbers-between-different-bases-in-javascript
  * 
+ * specifically: https://stackoverflow.com/a/55011290/1277651
  */
-function convertBase(value, from_base, to_base) {
-    var range = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.-'.split(''); //
-    var from_range = range.slice(0, from_base);
-    var to_range = range.slice(0, to_base);
-    
-    var dec_value = value.split('').reverse().reduce(function (carry, digit, index) {
-      if (from_range.indexOf(digit) === -1) throw new Error('Invalid digit `'+digit+'` for base '+from_base+'.');
-      return carry += from_range.indexOf(digit) * (Math.pow(from_base, index));
-    }, 0);
-    
-    var new_value = '';
-    while (dec_value > 0) {
-      new_value = to_range[dec_value % to_base] + new_value;
-      dec_value = (dec_value - (dec_value % to_base)) / to_base;
-    }
-    return new_value || '0';
-  }
 
-
-
-//https://stackoverflow.com/a/55011290/1277651
-  function convertBase2(str, fromBase, toBase) {
+function convertBase(str, fromBase, toBase) {
 
     const DIGITS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!*'();:@&=+$,/?#[]-_.~" + //26 + 26 + 10 + 22 = 84
 
@@ -100,131 +81,63 @@ function convertBase(value, from_base, to_base) {
 
     //total = 1090
 
-    //"😀😁😂🤣😃😄😅😆😉😊😋😎😍😘🥰😗😙😚☺️🙂🤗🤩🤔🤨😐😑😶🙄😏😣😥😮🤐😯😪😫😴😌😛😜😝🤤😒😓😔😕🙃🤑😲☹️🙁😖😞😟😤😢😭😦😧😨😩🤯😬😰😱🥵🥶😳🤪😵😡😠🤬😷🤒🤕🤢🤮🤧😇🤠🤡🥳🥴🥺🤥🤫🤭🧐🤓😈👿👹👺💀👻👽🤖💩😺😸😹😻😼😽🙀😿😾"; //110
-    //
-
-    DIGITS
-
+    
     const add = (x, y, base) => {
-        let z = [];
-        const n = Math.max(x.length, y.length);
-        let carry = 0;
-        let i = 0;
-        while (i < n || carry) {
-            const xi = i < x.length ? x[i] : 0;
-            const yi = i < y.length ? y[i] : 0;
-            const zi = carry + xi + yi;
-            z.push(zi % base);
-            carry = Math.floor(zi / base);
-            i++;
-        }
-        return z;
-    }
-
-    const multiplyByNumber = (num, x, base) => {
-        if (num < 0) return null;
-        if (num == 0) return [];
-
-        let result = [];
-        let power = x;
-        while (true) {
-            num & 1 && (result = add(result, power, base));
-            num = num >> 1;
-            if (num === 0) break;
-            power = add(power, power, base);
-        }
-
-        return result;
-    }
-
-    const parseToDigitsArray = (str, base) => {
-        const digits = str.split('');
-        let arr = [];
-        for (let i = digits.length - 1; i >= 0; i--) {
-            const n = DIGITS.indexOf(digits[i])
-            if (n == -1) return null;
-            arr.push(n);
-        }
-        return arr;
-    }
-
-    const digits = parseToDigitsArray(str, fromBase);
-    if (digits === null) return null;
-
-    let outArray = [];
-    let power = [1];
-    for (let i = 0; i < digits.length; i++) {
-        digits[i] && (outArray = add(outArray, multiplyByNumber(digits[i], power, toBase), toBase));
-        power = multiplyByNumber(fromBase, power, toBase);
-    }
-
-    let out = '';
-    for (let i = outArray.length - 1; i >= 0; i--)
-        out += DIGITS[outArray[i]];
-
-    return out;
-}
-
-
-
-
-
-  
-
-  // LZW-compress a string
-function lzw_encode(s) {
-  var dict = {};
-  var data = (s + "").split("");
-  var out = [];
-  var currChar;
-  var phrase = data[0];
-  var code = 256;
-  for (var i=1; i<data.length; i++) {
-      currChar=data[i];
-      if (dict[phrase + currChar] != null) {
-          phrase += currChar;
+      let z = [];
+      const n = Math.max(x.length, y.length);
+      let carry = 0;
+      let i = 0;
+      while (i < n || carry) {
+          const xi = i < x.length ? x[i] : 0;
+          const yi = i < y.length ? y[i] : 0;
+          const zi = carry + xi + yi;
+          z.push(zi % base);
+          carry = Math.floor(zi / base);
+          i++;
       }
-      else {
-          out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-          dict[phrase + currChar] = code;
-          code++;
-          phrase=currChar;
-      }
+      return z;
   }
-  out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-  for (var i=0; i<out.length; i++) {
-      out[i] = String.fromCharCode(out[i]);
-  }
-  return out.join("");
-}
 
-// Decompress an LZW-encoded string
-function lzw_decode(s) {
-  var dict = {};
-  var data = (s + "").split("");
-  var currChar = data[0];
-  var oldPhrase = currChar;
-  var out = [currChar];
-  var code = 256;
-  var phrase;
-  for (var i=1; i<data.length; i++) {
-      var currCode = data[i].charCodeAt(0);
-      if (currCode < 256) {
-          phrase = data[i];
+  const multiplyByNumber = (num, x, base) => {
+      if (num < 0) return null;
+      if (num == 0) return [];
+
+      let result = [];
+      let power = x;
+      while (true) {
+          num & 1 && (result = add(result, power, base));
+          num = num >> 1;
+          if (num === 0) break;
+          power = add(power, power, base);
       }
-      else {
-         phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
-      }
-      out.push(phrase);
-      currChar = phrase.charAt(0);
-      dict[code] = oldPhrase + currChar;
-      code++;
-      oldPhrase = phrase;
+
+      return result;
   }
-  return out.join("");
+
+  const parseToDigitsArray = (str, base) => {
+      const digits = str.split('');
+      let arr = [];
+      for (let i = digits.length - 1; i >= 0; i--) {
+          const n = DIGITS.indexOf(digits[i])
+          if (n == -1) return null;
+          arr.push(n);
+      }
+      return arr;
+  }
+
+  const digits = parseToDigitsArray(str, fromBase);
+  if (digits === null) return null;
+
+  let outArray = [];
+  let power = [1];
+  for (let i = 0; i < digits.length; i++) {
+      digits[i] && (outArray = add(outArray, multiplyByNumber(digits[i], power, toBase), toBase));
+      power = multiplyByNumber(fromBase, power, toBase);
+  }
+
+  let out = '';
+  for (let i = outArray.length - 1; i >= 0; i--)
+      out += DIGITS[outArray[i]];
+
+  return out;
 }
-
-function en(c){var x='charCodeAt',b,e={},f=c.split(""),d=[],a=f[0],g=256;for(b=1;b<f.length;b++)c=f[b],null!=e[a+c]?a+=c:(d.push(1<a.length?e[a]:a[x](0)),e[a+c]=g,g++,a=c);d.push(1<a.length?e[a]:a[x](0));for(b=0;b<d.length;b++)d[b]=String.fromCharCode(d[b]);return d.join("")}
-
-function de(b){var a,e={},d=b.split(""),c=f=d[0],g=[c],h=o=256;for(b=1;b<d.length;b++)a=d[b].charCodeAt(0),a=h>a?d[b]:e[a]?e[a]:f+c,g.push(a),c=a.charAt(0),e[o]=f+c,o++,f=a;return g.join("")}
-
