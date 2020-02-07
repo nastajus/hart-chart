567f6c7ae3a1d6e2f00a54acfdaa37ba6d1a6f93
Ian Nastajus <ian.nastajus@gmail.com>
2020-02-07 15:16:33 -0500

    share link works, has toast notif...
    can manually delete from address bar the params now...

--- src/eye-charts.js
+++ src/eye-charts.js
@@ -72,60 +72,11 @@ function uniqueInOrder(x) {
 
 function updateUrlAndHistory(theLetters) {
   
-    //offline when opened *directly* in browser *without* web host:
-    //returns: 
-    /*
-    origin: "file://"
-    protocol: "file:"
-    host: ""
-    hostname: ""
-    port: ""
-    pathname: "/C:/Users/GMunion/Documents/GitHub/eye-chart/index.html"
-    search: ""
-    hash: ""
-    href: "file:///C:/Users/GMunion/Documents/GitHub/eye-chart/index.html"
-    */
 
-    /*
-    to test still: offline with localhost? protocol... 
-    */
-
-    //online:
-    /*
-    origin: "https://nastajus.github.io"
-    protocol: "https:"
-    host: "nastajus.github.io"
-    hostname: "nastajus.github.io"
-    port: ""
-    pathname: "/hart-chart/"
-    search: ""
-    hash: ""
-    href: "https://nastajus.github.io/hart-chart/"
-    */
+    var fullPath = addParm('q', theLetters); 
+    return fullPath;
 
 
-    console.log(window.location);
-    //experimentsInParams();
-    addParm('q', theLetters); 
-
-
-    /* 
-     * wonderful source: 
-     * https://stackoverflow.com/questions/35254564/javascript-window-location-not-working
-     */
-
-    //if (window.location.protocol.includes("file")) { }
-    switch (window.location.protocol) {
-      case "file:":
-       
-        break;
-      case "http:":
-      case "https:":
-        //pathname: "/hart-chart/" (at least on Github)
-        break;
-      default:
-        //
-    }
 }
 
 
@@ -136,7 +87,13 @@ function fetchUrlParams() {
     var searchParams = new URLSearchParams(window.location.search);
     var result = searchParams.get('q');
     return result;
+  }
+}
 
+//not thoroughly tested...
+function fetchFullUrl() {
+  if ('URLSearchParams' in window) {
+    return window.location.pathname + '?' + window.location.search;
   }
 }
 
@@ -160,6 +117,7 @@ function addParm(key, val) {
     //won't cause page load:
     var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
     history.pushState(null, '', newRelativePathQuery);
+    return newRelativePathQuery; 
 
   }
 
@@ -226,120 +184,4 @@ function copyTextToClipboard(text) {
   }
 
   document.body.removeChild(textArea);
-}
-
-
-
-
-
-
-/**
- * delete this soon
- */
-function experimentsInParams() {
-   //pathname: "/C:/Users/GMunion/Documents/GitHub/eye-chart/index.html" (at least on my Windows)
-        // Pass in a string literal
-
-        //var url = new URL('https://example.com?foo=1&bar=2');
-        //var url = new URL(window.location.href);
-        //var url = new URL(window.location);
-        var url = new URL('https://example.com?foo=1&bar=2');
-
-        // Retrieve params via url.search, passed into ctor
-        //var params = new URLSearchParams();
-
-        //const params = new URLSearchParams();
-        //const params = new URLSearchParams(window.location.search);
-        var params = new URLSearchParams(window.location.search);
-        params.append('api_key', '1');
-        params.append('foo', '1');
-        params.append('baR', '122');
-        params.append('api_key', '1');
-
-        //TODO: better understand access restrictions possible within JS objects...
-        //eg why can't i see contents of this, and how can i build an equivalent myself, and verify if 
-        console.dir(url.search); 
-        console.dir(url.searchParams); 
-        console.dir(params); 
-        console.dir(url.searchParams.toString()); 
-        console.dir(params.toString()); 
-
-        console.log(params.get('bar')); //returns null
-        console.log(params.get('baR')); //retuns 122 ... ok!!! cool.
-        console.log(params.entries); //does nothing useful... shows "f entries() { [native code] }"
-        console.log(params.searchParams); //returns undefined... because non-sensical. :/
-          //vscode lesson: if it "autocompletes" with preceding icon "abc" it's a garbage-autocomp.
-          //instead i should scroll down after typing . to the icons "purple isobox" for methods.
-          //those are the actual intellisense operating i suspect.
-
-          //params.searchParams.entries.entries.entries; //auto intellisense garbage. //sigh.
-
-          console.log(params.keys) //returns "f entries() { [native code] }"
-          console.log(params.values) //returns "f entries() { [native code] }"
-          console.log(params.getAll ) //returns "f entries() { [native code] }"
-
-          console.log(params.keys()) //returns "Iterator {} -i-" >> __proto__...
-          console.log(params.values()) //returns "Iterator {} -i-"
-          console.log(params.getAll() ) //returns "Uncaught TypeError: Failed to execute 'getAll' on 'URLSearchParams': 1 argument required, but only 0 present."    
-
-        
-          //ok, there's lots at play here..
-          //i keep delving down the rabbit hole...
-
-
-
-        // Create a test URLSearchParams object
-        var searchParams = new URLSearchParams("key1=value1&key2=value2");
-
-
-
-        //var allParams = [];
-        var allParams = {};
-        // Log the values
-        //searchParams.forEach(function(value, key) {
-        params.forEach(function(value, keyy) {
-          console.log(value, keyy); // how do i build a list inside a callback and return it?... i dont'... right? by side-effect of declaring a var in the outside-scope and appending inside? one, that stinks, two that may not actually work. //possibly isn't, in this case... wow, there's so much to know still about web dev.
-          
-          //allParams.push({keyy, value});
-          //allParams.push({keyy : value });
-          allParams[keyy] = value;
-        });
-
-        console.log("allParams", allParams);
-
-        /*
-        formerly printed out... when using  allParams.push({keyy, value});...
-        0: {key: "api_key", value: "1"}
-        1: {key: "foo", value: "1"}
-        2: {key: "baR", value: "122"}
-        3: {key: "api_key", value: "1"
-         */
-
-        if (allParams["api_key"]) {
-          console.log("ya")
-        }
-        if (allParams["api_keyd"]) {
-          console.log("yaaaa")
-        }
-
-        
-        if (allParams.api_key) {
-          console.log("yqqa")
-        }
-        if (allParams.api_keyd) {
-          console.log("yaaqqaa")
-        }
-
-
-        // note: params can also be directly created
-// let url = new URL('https://example.com?foo=1&bar=2');
-// let params = url.searchParams;
-
-// // or even simpler
-// let params = new URLSearchParams('foo=1&bar=2');
-
-
-        // Pass in a record
-        var params4 = new URLSearchParams({"foo" : 1 , "bar" : 2});
-
 }
