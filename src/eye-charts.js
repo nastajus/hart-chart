2e28e203025a4ac150a3dbb227cc34176410be2f
Ian Nastajus <ian.nastajus@gmail.com>
2020-02-08 17:59:03 -0500

    get font width... disables 50px size...

--- src/eye-charts.js
+++ src/eye-charts.js
@@ -192,3 +192,41 @@ function copyTextToClipboard(text) {
 
   document.body.removeChild(textArea);
 }
+
+
+
+
+
+/** 
+ * Uses canvas.measureText to compute and return the width of the given text of given font in pixels.
+ * 
+ * @param {String} text The text to be rendered.
+ * @param {String} font The css font descriptor that text is to be rendered with (e.g. "bold 14px verdana").
+ * 
+ * Courtesy of: 
+ * @see https://stackoverflow.com/questions/118241/calculate-text-width-with-javascript/21015393#21015393
+ */
+function getTextWidth(text, font) {
+  // re-use canvas object for better performance
+  var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+  var context = canvas.getContext("2d");
+  context.font = font;
+  var metrics = context.measureText(text);
+  return metrics.width;
+}
+
+//console.log(getTextWidth("hello there!", "bold 12pt arial")); 
+
+
+
+/**
+ * Courtesy of: 
+ * https://stackoverflow.com/questions/566203/changing-css-values-with-javascript
+ */
+function changeCss () 
+{
+  var cssRuleCode = document.all ? 'rules' : 'cssRules'; //account for IE and FF
+  var rule = document.styleSheets[styleIndex][cssRuleCode][ruleIndex];
+  var selector = rule.selectorText;  //maybe '#tId'
+  var value = rule.value;            //both selectorText and value are settable.
+}
